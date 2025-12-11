32b1c3dc6d1432592bde82dcc54915c66313b519
FadedShadow17 <144658664+FadedShadow17@users.noreply.github.com>
2025-12-11 10:08:33 +0545

    Bottom Navigation Screen Added

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:hamro_service/bottom_navigation/about.dart';
+import 'package:hamro_service/bottom_navigation/cart.dart';
+import 'package:hamro_service/bottom_navigation/home.dart';
+import 'package:hamro_service/bottom_navigation/profile.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
   @override
+  State<Dashboard> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<Dashboard> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const Home(),
+    const Cart(),
+    const Profile(),
+    const About(),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F5F5),
-      body: const Center(
-        child: Text(
-          'welcome to dashboard',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w500,
-            color: Colors.black87,
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 25, 28, 237),
+      ),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: "Cart",
           ),
-        ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.album_outlined),
+            label: "About",
+          ),
+        ],
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.lightGreen,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
 }
-
-
