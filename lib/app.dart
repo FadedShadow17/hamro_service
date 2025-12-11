aaeeb1b0359dabb8a347b4fd7402a2f119c4f877
FadedShadow17 <144658664+FadedShadow17@users.noreply.github.com>
2025-12-11 19:36:52 +0545

    Theme fixes

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // import 'package:hamro_service/screens/onboarding/onboarding_screen.dart';
 // import 'package:hamro_service/screens/signup_screen.dart';
 import 'package:hamro_service/screens/splash_screen.dart';
+import 'package:hamro_service/themes/themes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,6 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
       home: const SplashScreen(),
     );
   }
